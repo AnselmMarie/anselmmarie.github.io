d94658c22137c0c7190bd643b4f5af0160fe5eda
Anselm Marie <ansmarie@gmail.com>
2019-05-01 15:58:35 -0400

    Updates

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# ansmarie.github.io
---
+++ precache-manifest.481d8f3ec45f211c3bbb075a30306e14.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf03b17b2aaba40d91800e8b636d86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29e936c26513736d67c0",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "209e1e7c0aad4007bd2f",
+    "url": "/static/css/main.2a1e55e5.chunk.css"
+  },
+  {
+    "revision": "29e936c26513736d67c0",
+    "url": "/static/js/2.71ea6741.chunk.js"
+  },
+  {
+    "revision": "209e1e7c0aad4007bd2f",
+    "url": "/static/js/main.30fe5a7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abccd3ebac74ab3f988f0d487fa43245",
+    "url": "/static/media/ADAM.CGPRO.abccd3eb.ttf"
+  },
+  {
+    "revision": "b1ec72771fe5b705553ae55550e5d1b0",
+    "url": "/static/media/ADAM.CGPRO.b1ec7277.woff"
+  },
+  {
+    "revision": "cc1d83289ca51eecd3937304bc682987",
+    "url": "/static/media/ADAM.CGPRO.cc1d8328.woff2"
+  },
+  {
+    "revision": "11dfd3c4cb598686c6a7bd2e6f2375ec",
+    "url": "/static/media/app-thumbnail.11dfd3c4.jpg"
+  },
+  {
+    "revision": "f5136dc1cade34847d9b74a263bcf1b1",
+    "url": "/static/media/app01.f5136dc1.jpg"
+  },
+  {
+    "revision": "8289a5e220c92e0089c18ea186879660",
+    "url": "/static/media/app02.8289a5e2.jpg"
+  },
+  {
+    "revision": "78557c89655ffa42aa8c9eafdf300f9b",
+    "url": "/static/media/app03.78557c89.jpg"
+  },
+  {
+    "revision": "41b6268928a9b6fc72e41bc7cdcfc798",
+    "url": "/static/media/breezeThru-thumbnail.41b62689.jpg"
+  },
+  {
+    "revision": "d291e2de739c74e451a8feeb78f9f23f",
+    "url": "/static/media/breezeThru01.d291e2de.jpg"
+  },
+  {
+    "revision": "54574732251cafb14a71368bc573fe73",
+    "url": "/static/media/breezeThru02.54574732.jpg"
+  },
+  {
+    "revision": "c2a0e08f293f628e4982d13f0ea15f1b",
+    "url": "/static/media/breezeThru03.c2a0e08f.jpg"
+  },
+  {
+    "revision": "d06b68c3f2de9129e7c3495402710cc4",
+    "url": "/static/media/breezeThru04.d06b68c3.jpg"
+  },
+  {
+    "revision": "6c4a66c9f3bf8702af69e593dcfa7fb1",
+    "url": "/static/media/cat-thumbnail.6c4a66c9.jpg"
+  },
+  {
+    "revision": "9ed65920b83fb00619c8cda6108882d5",
+    "url": "/static/media/cat01.9ed65920.jpg"
+  },
+  {
+    "revision": "090e73ba7bd4f41b52b11a5fe46b3247",
+    "url": "/static/media/clorox-thumbnail.090e73ba.jpg"
+  },
+  {
+    "revision": "e5fed5f11e1d51f930f36107e96e4967",
+    "url": "/static/media/clorox01.e5fed5f1.jpg"
+  },
+  {
+    "revision": "462e8fc55bb54531c28ef117e6460e6c",
+    "url": "/static/media/clorox02.462e8fc5.jpg"
+  },
+  {
+    "revision": "2f80888a8286f39749cf1c5e55f29d50",
+    "url": "/static/media/clorox03.2f80888a.jpg"
+  },
+  {
+    "revision": "ef8a9cad6cdd20645fa21a08686f6450",
+    "url": "/static/media/cosajiou-details.ef8a9cad.jpg"
+  },
+  {
+    "revision": "21e32a4b0cc71c81e8b47d5fbba7ebbd",
+    "url": "/static/media/cosajiou-form.21e32a4b.jpg"
+  },
+  {
+    "revision": "71f4e3f27a9c38ad82636d9b5eac06b9",
+    "url": "/static/media/cosajiou-login.71f4e3f2.jpg"
+  },
+  {
+    "revision": "6b175698b0e281954abed804ca771ed4",
+    "url": "/static/media/cosajiou-modal.6b175698.jpg"
+  },
+  {
+    "revision": "3cf20bb240f06fb29f9f28cd6ed69457",
+    "url": "/static/media/cosajiou-project-completed.3cf20bb2.jpg"
+  },
+  {
+    "revision": "de13fee638a47926a6ee4809e73787ae",
+    "url": "/static/media/cosajiou-projects.de13fee6.jpg"
+  },
+  {
+    "revision": "e4a3773eadf92b4f420c4c4262c69428",
+    "url": "/static/media/cosajiou-thumbnail.e4a3773e.jpg"
+  },
+  {
+    "revision": "8c5a4eb53e4720f1aaca45040d556e0c",
+    "url": "/static/media/cosajiou-whats-next.8c5a4eb5.jpg"
+  },
+  {
+    "revision": "3c49e5b653e3e5a0fdc73a92607d9aef",
+    "url": "/static/media/crunchyroll-thumbnail.3c49e5b6.jpg"
+  },
+  {
+    "revision": "cf5dbd11e2ecd32bbb9c9fb1edc162a2",
+    "url": "/static/media/crunchyroll01.cf5dbd11.jpg"
+  },
+  {
+    "revision": "6921d1a2dda686296381487643ac650a",
+    "url": "/static/media/crunchyroll02.6921d1a2.jpg"
+  },
+  {
+    "revision": "19d35d10f49225f8a3cd4216255ab917",
+    "url": "/static/media/darden-thumbnail.19d35d10.jpg"
+  },
+  {
+    "revision": "1910996443b827d47b92dd1f0155216b",
+    "url": "/static/media/darden01.19109964.jpg"
+  },
+  {
+    "revision": "aadc4ef18012f3f31e8971f99a5f5065",
+    "url": "/static/media/darden02.aadc4ef1.jpg"
+  },
+  {
+    "revision": "ee3a637e1d2590507ea49d1004c4b0d6",
+    "url": "/static/media/darden03.ee3a637e.jpg"
+  },
+  {
+    "revision": "34bf85e2a77d8d79d5c199b2762bcda7",
+    "url": "/static/media/darden04.34bf85e2.jpg"
+  },
+  {
+    "revision": "644c1fa3531c8d8eaedd7416498e7dad",
+    "url": "/static/media/email01.644c1fa3.jpg"
+  },
+  {
+    "revision": "87cb217106f88ff2f4ea5577bbcc73b7",
+    "url": "/static/media/email02.87cb2171.jpg"
+  },
+  {
+    "revision": "795e2991691fc2b13bc6dbb0e81dd97e",
+    "url": "/static/media/email03.795e2991.png"
+  },
+  {
+    "revision": "998e2dfc1634bb036e2539f05b7e45d8",
+    "url": "/static/media/filezilla-thumbnail.998e2dfc.jpg"
+  },
+  {
+    "revision": "3c13f4df0fe7461e19b8c563a7ce9079",
+    "url": "/static/media/hero-image.3c13f4df.jpg"
+  },
+  {
+    "revision": "55994da928b5700e9ce1ae476dca08f9",
+    "url": "/static/media/icon01.55994da9.jpg"
+  },
+  {
+    "revision": "aeec8e1fd1923f3b0f5439194500430d",
+    "url": "/static/media/instagram-thumbnail.aeec8e1f.jpg"
+  },
+  {
+    "revision": "207300fe2ca8726c647da0dd2e45e48a",
+    "url": "/static/media/instagram01.207300fe.jpg"
+  },
+  {
+    "revision": "5c8326c86ac54c8a8e013f8e9709ad2a",
+    "url": "/static/media/instagram02.5c8326c8.jpg"
+  },
+  {
+    "revision": "e2bfedcd6441d7d0c1be52608beec075",
+    "url": "/static/media/instagram03.e2bfedcd.jpg"
+  },
+  {
+    "revision": "6b2b7c62acb4d9f05aed1908267f7f8d",
+    "url": "/static/media/rebrand-thumbnail.6b2b7c62.jpg"
+  },
+  {
+    "revision": "b33af0d913e9ca1c4c2e4d4acf1d0afc",
+    "url": "/static/media/rebrand01.b33af0d9.jpg"
+  },
+  {
+    "revision": "e64568bf852887aff670924ad8dfdc4e",
+    "url": "/static/media/rebrand02.e64568bf.jpg"
+  },
+  {
+    "revision": "b9e5af207305489ec57a79a116bd05b3",
+    "url": "/static/media/rebrand03.b9e5af20.jpg"
+  },
+  {
+    "revision": "37a9f2193bac876f3bcb9bf5e19ce4d4",
+    "url": "/static/media/responsive01.37a9f219.jpg"
+  },
+  {
+    "revision": "bb0949e0002867b7b7198ca1853d8df4",
+    "url": "/static/media/responsive02.bb0949e0.jpg"
+  },
+  {
+    "revision": "3b3c304e322fd43f922be8abec36695c",
+    "url": "/static/media/responsive03.3b3c304e.jpg"
+  },
+  {
+    "revision": "ac8c5a9af45a5ad2938fda1f3dc72596",
+    "url": "/static/media/resume-developer.ac8c5a9a.pdf"
+  },
+  {
+    "revision": "00e2ad417462cdea1c166412ef19cd23",
+    "url": "/static/media/weatherApp01.00e2ad41.jpg"
+  },
+  {
+    "revision": "fb667d80ce943d0bc6a75fbbec7bc8b8",
+    "url": "/static/media/weatherApp02.fb667d80.jpg"
+  },
+  {
+    "revision": "0efcdc34e94689d507a933749a41346e",
+    "url": "/static/media/website01.0efcdc34.jpg"
+  },
+  {
+    "revision": "eff57e290f17d7119253ac9fc9cb3c17",
+    "url": "/static/media/website02.eff57e29.jpg"
+  },
+  {
+    "revision": "d7d469223f1d9c46696edb63122b1349",
+    "url": "/static/media/website03.d7d46922.jpg"
+  }
+]);
